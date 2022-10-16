f4851036f5cc1bd669557001c34c80854ca09f59
Susmita Karyakarte <skaryakarte23@gmail.com>
2022-10-16 10:17:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,62 +1,10 @@
 import streamlit as st
-import streamlit.components.v1 as components
-import pandas as pd
-import numpy as np
-    
-@st.cache(suppress_st_warning=True,allow_output_mutation=True)
-def app():
+from multiapp import MultiApp
 
-    st.title('Check your Approval for Home Buying')
-    st.write('Please enter some information below')
-    st.write('The system will predict if you are currently eligible for approval')
-    
-    st.write('All are required fields')
+app = MultiApp()
 
-    form = st.form(key='my_form')
-    GrossMonthlyIncome = form.number_input(label='Gross Monthly Income', min_value=10)
-    CreditCardPayment = form.number_input(label='Credit Card Payment', min_value=0)
-    CarPayment = form.number_input(label='Car Payment',min_value=0)
-    StudentLoanPayments = form.number_input(label='Student Loan Payments',min_value=0)
-    AppraisedValue = form.number_input(label='Appraised Value',min_value=0)
-    DownPayment = form.number_input(label='Down Payment',min_value=0)
-    LoanAmount = form.number_input(label='Loan Amount',min_value=0)
-    MonthlyMortgagePayment = form.number_input(label='Monthly Mortgage Payment',min_value=0)
-    CreditScore = form.slider(label="Credit Score",key = "CreditScore", min_value=0, max_value=1500)
-    
-    submit_button = form.form_submit_button(label='Submit')
+# Add all your application here
+app.add_app("Hey", form.app)
 
-    if submit_button or CreditScore:
-
-        
-
-        if CreditScore > 640:
-
-            loanbalance = AppraisedValue-DownPayment
-            ltv = (loanbalance/AppraisedValue)*100
-
-            if ltv > 80:
-               pmi = AppraisedValue/100
-
-            temp = CreditCardPayment+CarPayment+StudentLoanPayments+MonthlyMortgagePayment
-            dti = (temp / GrossMonthlyIncome)*100
-
-            if dti <= 43:
-               fedti = ((temp-MonthlyMortgagePayment)/GrossMonthlyIncome)*100
-
-               if fedti <= 28:
-
-                  
-
-                  st.write('Approved')
-
-               else:
-                    st.write('FEDTI score too high')
-
-            else:
-                 st.write('DTI too high')
-
-        else:
-            st.write('Credit Score too low')
-
-			
-app()
+# The main app
+app.run()
