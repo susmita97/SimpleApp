ff020ce81fe1fe7f7e6fab877037459ae3b9c366
Susmita Karyakarte <skaryakarte23@gmail.com>
2022-10-16 10:35:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from multiapp import MultiApp
 app = MultiApp()
 
 # Add all your application here
-app.add_app("Hey", form.app)
+app.add_app("Fill a form", form.app)
+app.add_app("For future features", feature.app)
 
 # The main app
 app.run()
